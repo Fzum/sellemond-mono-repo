0b93127e6285bbf4c611aa9b9c3acf306cd11ca1
fzum <sellemondfabian@gmail.com>
2022-08-03 20:48:46 +0200

    extract const for description

--- libs/portfolio/domain/src/lib/application/hero.facade.ts
+++ libs/portfolio/domain/src/lib/application/hero.facade.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { HeroContent } from '../entities/hero-content';
 
+const description =
+  "I'm Fabian, a freelancer passionate about software engineering. " +
+  'I am experienced in developing web applications starting from the user interface until all the way back to the database - the full stack. ';
+
 @Injectable({ providedIn: 'root' })
 export class HeroFacade {
   content$ = new BehaviorSubject<HeroContent>({
@@ -12,9 +16,8 @@ export class HeroFacade {
 
   loadHeroContent() {
     this.content$.next({
-      title: 'BEST PLACE TO CHOOSE YOUR CLOTHES',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.',
+      title: 'Welcome',
+      description,
       imageUrl: 'https://merakiui.com/_nuxt/img/Catalogue-pana.32658f1.svg',
     });
   }
