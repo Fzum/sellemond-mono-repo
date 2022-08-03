033314759efe5c209c1b2d3aed8bd31917232bde
fzum <sellemondfabian@gmail.com>
2022-08-03 22:34:12 +0200

    scaffold projects

--- libs/portfolio/feature-projects/src/lib/projects.component.ts
+++ libs/portfolio/feature-projects/src/lib/projects.component.ts
@@ -12,5 +12,30 @@ import { ProjectsFacade } from '@sellemond/portfolio/domain';
 export class ProjectsComponent implements OnInit {
   constructor(private projectsFacade: ProjectsFacade) {}
 
+  // array with three projects hard coded
+  projects: Project[] = [
+    {
+      name: 'Project 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      icon: 'assets/images/projects/project-1.jpg',
+    },
+    {
+      name: 'Project 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      icon: 'assets/images/projects/project-2.jpg',
+    },
+    {
+      name: 'Project 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      icon: 'assets/images/projects/project-3.jpg',
+    },
+  ];
+
   ngOnInit() {}
+}
+
+interface Project {
+  name: string;
+  description: string;
+  icon: string;
 }
