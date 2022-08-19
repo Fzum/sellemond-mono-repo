46e56582998658de2523bef962b802b21af221b8
fzum <sellemondfabian@gmail.com>
2022-08-19 15:24:24 +0200

    scaffold project display

--- libs/portfolio/feature-projects/src/lib/projects.component.ts
+++ libs/portfolio/feature-projects/src/lib/projects.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsFacade } from '@sellemond/portfolio/domain';
+import { UiCardComponent } from '../../../../shared/ui-components/src/lib/ui-card/ui-card.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UiCardComponent],
   selector: 'portfolio-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  projects = [1, 2, 3, 4, 5];
+
   constructor(private projectsFacade: ProjectsFacade) {}
 
   ngOnInit() {}
