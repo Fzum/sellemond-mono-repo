8d9af7226fed18be8399fe08c632e88a77d883f4
fabian <sellemondfabian@gmail.com>
2022-09-03 14:00:54 +0200

    Scaffold solution for dark mode toggle

--- apps/portfolio/src/app/app.component.ts
+++ apps/portfolio/src/app/app.component.ts
@@ -102,4 +102,8 @@ export class AppComponent implements OnDestroy {
   sanitize(svgHtml: string | undefined) {
     return this.sanitizer.bypassSecurityTrustHtml(svgHtml ?? '');
   }
+
+  toggleDarkMode()  {
+    document.documentElement.classList.toggle('dark');
+  }
 }
