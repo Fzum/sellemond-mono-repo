1fed0f54a2bb3aa7667d0dbafad509b9b24c8455
fabian <sellemondfabian@gmail.com>
2022-10-17 18:38:35 +0200

    close #5: fix typos

--- libs/portfolio/domain/src/lib/application/hero.facade.ts
+++ libs/portfolio/domain/src/lib/application/hero.facade.ts
@@ -4,12 +4,12 @@ import { HeroContent } from '../entities/hero-content.model';
 
 const description = `<p>I'm Fabian, a <span class="text-accent-dark decoration-accent-dark">Freelance Fullstack Java developer</span> passionate about software
   engineering and <span class="text-accent-dark decoration-accent-dark">clean code</span>. I am experienced in developing <span class="text-accent-dark decoration-accent-dark">web applications</span> starting from the user
-  interface to all the way back to the <span class="text-accent-dark decoration-accent-dark">database</span>.</p><br><p>My professional career started
-  in 2017 while I was making my <span class="text-accent-dark decoration-accent-dark">Bachelor Studies in IT and Telecommunication</span>. Since then,
+  interface all the way back to the <span class="text-accent-dark decoration-accent-dark">database</span>.</p><br><p>My professional career started
+  in 2017 while I was doing my <span class="text-accent-dark decoration-accent-dark">Bachelor Studies in IT and Telecommunication</span>. Since then,
   I could gain comprehensive insights in different projects and teams of all sizes.</p> <br><p>To intensify my knowledge
   I started a <span class="text-accent-dark decoration-accent-dark">Masters program in Software Engineering</span> that I could successfully pass in
   2020.</p><br><p>Take a look around to familiarize yourself with my competences and experiences and don't hesitate
-  contacting me when I could be a good fit in your team.</p>`;
+  contacting me if I could be a good fit in your team.</p>`;
 
 @Injectable({ providedIn: 'root' })
 export class HeroFacade {
