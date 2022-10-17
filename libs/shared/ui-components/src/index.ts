d958657a9f229288a6d5b93061df2ed295b0b166
fabian <sellemondfabian@gmail.com>
2022-10-17 21:26:58 +0200

    remove hero ui component since it is too concrete to be reused in other projects

--- libs/shared/ui-components/src/index.ts
+++ libs/shared/ui-components/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/ui-hero/ui-hero.component';
 export * from './lib/ui-icon-grid/ui-icon-grid.model';
 export * from './lib/ui-icon-grid/ui-icon-grid.component';
 
@@ -8,3 +7,4 @@ export * from './lib/ui-toggle-switch/ui-toggle-switch.component';
 export * from './lib/ui-card/ui-card.component';
 
 export * from './lib/ui-button/ui-button.component';
+export * from './lib/ui-heading/ui-heading.component';
